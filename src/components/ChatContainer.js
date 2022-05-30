45ca83226009fe21e951635ab53e2cfefb0b4f6b
catalin.festila <catafest76@gmail.com>
2022-05-30 14:42:50 +0300

    ... update files

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-unreachable */
+import ChatHeader from "./ChatHeader"
+import MatchesDiplay from "./MatchesDisplay"
+import ChatDisplay from "./ChatDisplay"
+
 const ChatContainer = () => {
-  return
-  ( 
+ return(
     <div className="chat-container">
+      
       <ChatHeader/>
       <div>
         <button className="option">Matches</button>
-        <button className="option">Chats</button>
+        <button className="option">Chat</button>
       </div>
       <MatchesDiplay/>
       <ChatDisplay/>
     </div>
-  )
+ )
 }
 
 export default ChatContainer
